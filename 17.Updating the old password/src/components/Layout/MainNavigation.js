cb1d32f04a2e0b76d2eab37fe60a76ba60a81907
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-21 19:10:39 +0530

    Updating the old password

---
+++ 17.Updating the old password/src/components/Layout/MainNavigation.js
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  const ctx=useContext(AuthContext);
+  const logoutHandler=()=>{
+    ctx.logout();
+  }
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          
+          {!ctx.isLoggedIn &&  (
+            <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          )}
+          {ctx.isLoggedIn && (
+            <li>
+            <Link to='/profile'>Profile</Link>
+           </li>
+           )}
+           {ctx.isLoggedIn && (
+           <li>
+           <button onClick={logoutHandler}>Logout</button>
+          </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
